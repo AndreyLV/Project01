caa48a0661963c3bf91f28379b5b4d1804bfbe85
AndreyLV <office@amtlav.ru>
2023-07-31 08:14:27 +0700

    rock_paper_scissors_bot v1

--- test.py
+++ test.py
@@ -1,11 +1,50 @@
-from environs import Env
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart, Text
+from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-env = Env()              # Создаем экземпляр класса Env
-env.read_env()           # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = '6202160443:AAEaFi5CCq1OzkFfnBIEIzN58jOxCHb3Q9Y'
 
-bot_token = env('BOT_TOKEN')     # Сохраняем значение переменной окружения в переменную bot_token
-admin_id = env.int('ADMIN_ID')   # Преобразуем значение переменной окружения к типу int
-                                    # и сохраняем в переменной admin_id
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
 
-print(bot_token)
-print(admin_id)
+# Создаем список списков с кнопками
+keyboard: list[list[KeyboardButton]] = [
+    [KeyboardButton(text=str(i)) for i in range(1, 4)],
+    [KeyboardButton(text=str(i)) for i in range(4, 7)]]
+
+keyboard.append([KeyboardButton(text='7')])
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    keyboard=keyboard,
+    resize_keyboard=True)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+# и отправлять в чат клавиатуру
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text='Чего кошки боятся больше?',
+                            reply_markup=my_keyboard)
+
+
+# Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
+@dp.message(Text(text='Собак 🦮'))
+async def process_dog_answer(message: Message):
+    await message.answer(text='Да, несомненно, кошки боятся собак. '
+                            'Но вы видели как они боятся огурцов?')
+
+
+# Этот хэндлер будет срабатывать на ответ "Огурцов 🥒" и удалять клавиатуру
+@dp.message(Text(text='Огурцов 🥒'))
+async def process_cucumber_answer(message: Message):
+    await message.answer(text='Да, иногда кажется, что огурцов '
+                            'кошки боятся больше')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
