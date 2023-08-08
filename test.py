ce1e95824e585326b69b9be32f5401a55ffee14c
AndreyLV <office@amtlav.ru>
2023-08-08 07:45:05 +0700

    1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Text
-from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
@@ -11,39 +11,61 @@ API_TOKEN: str = '6202160443:AAEaFi5CCq1OzkFfnBIEIzN58jOxCHb3Q9Y'
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
-# Создаем список списков с кнопками
-keyboard: list[list[KeyboardButton]] = [
-    [KeyboardButton(text=str(i)) for i in range(1, 4)],
-    [KeyboardButton(text=str(i)) for i in range(4, 7)]]
+LEXICON: dict[str, str] = {'but_1': '1',
+                           'but_2': '2',
+                           'but_3': '3',
+                           'but_4': '4',
+                           'but_5': '5'}
 
-keyboard.append([KeyboardButton(text='7')])
 
-# Создаем объект клавиатуры, добавляя в него кнопки
-my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-    keyboard=keyboard,
-    resize_keyboard=True)
+BUTTONS: dict[str, str] = {'btn_1': '1',
+                           'btn_2': '2',
+                           'btn_3': '3',
+                           'btn_4': '4',
+                           'btn_5': '5'}
+
+
+# Функция для генерации инлайн-клавиатур "на лету"
+def create_inline_kb(width: int,
+                     *args: str,
+                     last_btn: str | None = None,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+    # Добавляем в билдер последнюю кнопку, если она передана в функцию
+    if last_btn:
+        kb_builder.row(InlineKeyboardButton(
+                            text=last_btn,
+                            callback_data='last_btn'))
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
 # и отправлять в чат клавиатуру
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(text='Чего кошки боятся больше?',
-                            reply_markup=my_keyboard)
-
-
-# Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
-@dp.message(Text(text='Собак 🦮'))
-async def process_dog_answer(message: Message):
-    await message.answer(text='Да, несомненно, кошки боятся собак. '
-                            'Но вы видели как они боятся огурцов?')
-
-
-# Этот хэндлер будет срабатывать на ответ "Огурцов 🥒" и удалять клавиатуру
-@dp.message(Text(text='Огурцов 🥒'))
-async def process_cucumber_answer(message: Message):
-    await message.answer(text='Да, иногда кажется, что огурцов '
-                            'кошки боятся больше')
+    keyboard = create_inline_kb(3, 'but_1', 'but_2', 'but_3', 'but_4', 'but_5', last_btn='Последняя кнопка')
+    await message.answer(text='Вот такая получается клавиатура',
+                        reply_markup=keyboard)
 
 
 if __name__ == '__main__':
